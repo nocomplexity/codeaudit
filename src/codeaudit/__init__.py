30b603c9f3147f5a84070a604d72de679b455be9
jurgenwigg <jurgenwigg@protonmail.com>
2026-04-10 20:21:13 +0200

    updated version

--- src/codeaudit/__init__.py
+++ src/codeaudit/__init__.py
@@ -1,4 +1,6 @@
 # SPDX-FileCopyrightText: 2025-present Maikel Mardjan - https://nocomplexity.com/
 #
 # SPDX-License-Identifier: GPL-3.0-or-later
-from .__about__ import __version__
+from importlib.metadata import version
+
+__version__ = version('codeaudit')
